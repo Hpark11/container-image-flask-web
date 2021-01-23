6cc0b867d88bdd8ece5fabac4d0a0537d1756367
Hpark11 <qkrgustnrk@gmail.com>
2021-01-23 22:06:42 +0900

    added \n

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
 @app.route("/")
 def main():
-    return "Hello, World!"
+    return "Hello, World!\n"
 
 @app.route('/respond')
 def niceToMeetYou():
-    return 'Nice to meet you'
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    return 'Nice to meet you\n'
